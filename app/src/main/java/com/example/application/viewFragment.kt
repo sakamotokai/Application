95cb26aa743df536cdf59a869cf0e860da897aaa
sakamotokai <Svorl1029384756k@gmail.com>
2022-06-19 14:52:39 +0300

    Step 5

--- app/src/main/java/com/example/application/viewFragment.kt
+++ app/src/main/java/com/example/application/viewFragment.kt
@@ -3,14 +3,13 @@ package com.example.application
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.application.databinding.FragmentBitcointBinding
-import com.example.application.databinding.FragmentDetetBinding
 
 
 class viewFragment:Fragment() {
-    fun showCourse(binding: FragmentDetetBinding, coin:String){
+    fun showCourse(binding: FragmentBitcointBinding, coin:String){
         getApiData().getData_bitcoin(binding.bitcoinCourseOfBitcoin, coin)
     }
-    fun showDopInfoCoin(binding: FragmentDetetBinding, coin:String){
+    fun showDopInfoCoin(binding: FragmentBitcointBinding, coin:String){
         getApiData().getDopData_bitcoin(binding.bitcoinDoubleInfo, coin)
     }
     fun showCatInfo(text:TextView){
